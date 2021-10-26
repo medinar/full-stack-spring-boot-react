dc6756afeda534a3eda0b06e4fb9e5efdae67aab
Mhel Medina <rommel.d.medina@gmail.com>
2021-10-25 22:00:32 -0700

    use Layout Component

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
 import { useState, useEffect } from "react";
 import { getAllStudents } from "./Client";
+import { Layout, Menu, Breadcrumb } from 'antd';
+import {
+    DesktopOutlined,
+    PieChartOutlined,
+    FileOutlined,
+    TeamOutlined,
+    UserOutlined,
+} from '@ant-design/icons';
+
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
 
 function App() {
     const [students, setStudents] = useState([]);
-
+    const [collapsed, setCollapsed] = useState(false);
     const fetchStudents = () =>
         getAllStudents()
             .then(res => res.json())
@@ -22,9 +33,45 @@ function App() {
         return "no data";
     }
 
-    return students.map((student, index) => {
-        return <p key={index}>{student.id} {student.name}</p>
-    })
+    return <Layout style={{ minHeight: '100vh' }}>
+                    <Sider collapsible collapsed={collapsed}
+                           onCollapse={setCollapsed}>
+                        <div className="logo" />
+                        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+                            <Menu.Item key="1" icon={<PieChartOutlined />}>
+                                Option 1
+                            </Menu.Item>
+                            <Menu.Item key="2" icon={<DesktopOutlined />}>
+                                Option 2
+                            </Menu.Item>
+                            <SubMenu key="sub1" icon={<UserOutlined />} title="User">
+                                <Menu.Item key="3">Tom</Menu.Item>
+                                <Menu.Item key="4">Bill</Menu.Item>
+                                <Menu.Item key="5">Alex</Menu.Item>
+                            </SubMenu>
+                            <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
+                                <Menu.Item key="6">Team 1</Menu.Item>
+                                <Menu.Item key="8">Team 2</Menu.Item>
+                            </SubMenu>
+                            <Menu.Item key="9" icon={<FileOutlined />}>
+                                Files
+                            </Menu.Item>
+                        </Menu>
+                    </Sider>
+                    <Layout className="site-layout">
+                        <Header className="site-layout-background" style={{ padding: 0 }} />
+                        <Content style={{ margin: '0 16px' }}>
+                            <Breadcrumb style={{ margin: '16px 0' }}>
+                                <Breadcrumb.Item>User</Breadcrumb.Item>
+                                <Breadcrumb.Item>Bill</Breadcrumb.Item>
+                            </Breadcrumb>
+                            <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+                                Bill is a cat.
+                            </div>
+                        </Content>
+                        <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+                    </Layout>
+                </Layout>;
 }
 
 export default App;
