69c0340837387b68947daeb3967509a0e006bfea
Mhel Medina <rommel.d.medina@gmail.com>
2021-10-25 20:39:54 -0700

    configure antd

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import { Button, Radio } from 'antd';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello React!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button type='primary'>Hello</Button>
+        <br/>
+        <Radio.Group value="large">
+            <Radio.Button value="larger">Large</Radio.Button>
+            <Radio.Button value="default">Default</Radio.Button>
+            <Radio.Button value="small">Small</Radio.Button>
+        </Radio.Group>
     </div>
   );
 }
