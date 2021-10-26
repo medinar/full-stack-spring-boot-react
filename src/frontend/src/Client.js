882a58d1f34452a59f75f040d633fcd33a9fbbae
Mhel Medina <rommel.d.medina@gmail.com>
2021-10-25 20:53:07 -0700

    implement getAllStatus

--- src/frontend/src/Client.js
+++ src/frontend/src/Client.js
@@ -1,1 +1,17 @@
 import fetch from 'unfetch';
+
+function checkStatus(response) {
+    if (response.ok) {
+        return response;
+    }
+    // convert non-2xx HTTP responses into errors
+    // else {
+    const error = new Error(response.statusText);
+    error.response = response;
+    return Promise.reject(error);
+    // }
+}
+
+export const getAllStudents = () =>
+    fetch("http://localhost:8080/api/v1/students")
+        .then(checkStatus);
