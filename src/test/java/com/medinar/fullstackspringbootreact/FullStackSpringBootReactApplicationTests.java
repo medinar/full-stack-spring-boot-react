0786bfe9e738f0ab5e2f06fb1529d05ba2bbe43a
Mhel Medina <rommel.d.medina@gmail.com>
2021-10-27 23:06:45 -0700

    add test using assertj and BDD approach

--- src/test/java/com/medinar/fullstackspringbootreact/FullStackSpringBootReactApplicationTests.java
+++ src/test/java/com/medinar/fullstackspringbootreact/FullStackSpringBootReactApplicationTests.java
@@ -1,15 +1,32 @@
 package com.medinar.fullstackspringbootreact;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class FullStackSpringBootReactApplicationTests {
 
+    Calculator underTest = new Calculator();
+
     @Test
-    void contextLoads() {
-//        Assertions.fail("Ooops test failed");
+    void itShouldAddTwoNumbers() {
+        // given
+        int numberOne = 20;
+        int numberTwo = 30;
+
+        // when
+        int result = underTest.add(numberOne, numberTwo);
+
+        // then
+        int expected = 50;
+        assertThat(result).isEqualTo(expected);
     }
 
+    class Calculator {
+        int add(int a, int b) {
+            return a + b;
+        }
+    }
 }
