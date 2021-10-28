d04e59803013abcf5bcf61c2bfe4b1452a97eb53
Mhel Medina <rommel.d.medina@gmail.com>
2021-10-28 00:20:22 -0700

    add test for getAllStudents method

---
+++ src/test/java/com/medinar/fullstackspringbootreact/student/StudentServiceTest.java
@@ -0,0 +1,41 @@
+package com.medinar.fullstackspringbootreact.student;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class StudentServiceTest {
+
+    @Mock
+    private StudentRepository studentRepository;
+    private StudentService underTest;
+
+    @BeforeEach
+    void setUp() {
+        underTest = new StudentService(studentRepository);
+    }
+
+    @Test
+    void canGetAllStudents() {
+        // when
+        underTest.getAllStudents();
+        // then
+        verify(studentRepository).findAll();
+    }
+
+    @Test
+    @Disabled
+    void addStudent() {
+    }
+
+    @Test
+    @Disabled
+    void deleteStudent() {
+    }
+}
