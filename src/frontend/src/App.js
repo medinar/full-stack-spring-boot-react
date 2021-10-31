584cd1720f71520e47a8eaa704afec437703f13c
Mhel Medina <rommel.d.medina@gmail.com>
2021-10-31 15:04:41 -0700

    add header

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
-import {useEffect, useState} from "react";
+import {Fragment, useEffect, useState} from "react";
 import {deleteStudent, getAllStudents} from "./Client";
 import {
     Avatar,
     Badge,
     Breadcrumb,
-    Button,
+    Button, Descriptions,
     Divider,
     Empty,
     Layout,
-    Menu,
+    Menu, PageHeader,
     Popconfirm,
     Radio,
     Spin,
@@ -187,9 +187,6 @@ function App() {
                 bordered
                 title={() =>
                     <>
-                        <Tag style={{marginLeft: "10px"}}>Number of students</Tag>
-                        <Badge count={students.length} className="site-badge-count-4" />
-                        <br/><br/>
                         <Button
                             onClick={() => setShowDrawer(!showDrawer)}
                             type="primary" shape="round" icon={<PlusCircleOutlined/>} size="small">
@@ -203,63 +200,79 @@ function App() {
             />
         </>
     }
-    return <Layout style={{minHeight: '100vh'}}>
-        <Sider collapsible collapsed={collapsed}
-               onCollapse={setCollapsed}>
-            <div className="logo"/>
-            <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-                <Menu.Item key="1" icon={<PieChartOutlined/>}>
-                    Option 1
-                </Menu.Item>
-                <Menu.Item key="2" icon={<DesktopOutlined/>}>
-                    Option 2
-                </Menu.Item>
-                <SubMenu key="sub1" icon={<UserOutlined/>} title="User">
-                    <Menu.Item key="3">Rommel</Menu.Item>
-                    <Menu.Item key="4">Tom</Menu.Item>
-                    <Menu.Item key="5">Bill</Menu.Item>
-                    <Menu.Item key="6">Alex</Menu.Item>
-                </SubMenu>
-                <SubMenu key="sub2" icon={<TeamOutlined/>} title="Team">
-                    <Menu.Item key="7">Admin</Menu.Item>
-                    <Menu.Item key="8">Registrar</Menu.Item>
-                </SubMenu>
-                <Menu.Item key="9" icon={<FileOutlined/>}>
-                    Files
-                </Menu.Item>
-            </Menu>
-        </Sider>
-        <Layout className="site-layout">
-            <Header className="site-layout-background" style={{padding: 0}}/>
-            <Content style={{margin: '0 16px'}}>
-                <Breadcrumb style={{margin: '16px 0'}}>
-                    <Breadcrumb.Item>User</Breadcrumb.Item>
-                    <Breadcrumb.Item>Rommel</Breadcrumb.Item>
-                </Breadcrumb>
-                <div className="site-layout-background" style={{padding: 24, minHeight: 360}}>
-                    {renderStudents()}
-                </div>
-            </Content>
-            <Footer style={{textAlign: 'center'}}>
-                <strong>by medinar <CodeOutlined style={{ fontSize: '18px', color: '#000' }}/></strong> ©2021
-                <Divider />
-                <a href="https://www.linkedin.com/in/rommel-medina-17bb9b30/"
-                   target="_blank"
-                   rel="noreferrer noopener"
-                   style={{margin: '5px'}}
-                >
-                     <LinkedinOutlined style={{ fontSize: '24px', marginwidth: '10px',color: '#000' }}/>
-                </a>
-                <a href="https://github.com/medinar"
-                   target="_blank"
-                   rel="noreferrer noopener"
-                   style={{margin: '5px'}}
-                >
-                     <GithubOutlined style={{ fontSize: '24px', color: '#000' }}/>
-                </a>
-            </Footer>
-        </Layout>
-    </Layout>
+    return <Fragment>
+                <Layout style={{minHeight: '100vh'}}>
+                    <Sider collapsible collapsed={collapsed}
+                           onCollapse={setCollapsed}>
+                        <div className="logo"/>
+                        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+                            <Menu.Item key="1" icon={<PieChartOutlined/>}>
+                                Option 1
+                            </Menu.Item>
+                            <Menu.Item key="2" icon={<DesktopOutlined/>}>
+                                Option 2
+                            </Menu.Item>
+                            <SubMenu key="sub1" icon={<UserOutlined/>} title="User">
+                                <Menu.Item key="3">Rommel</Menu.Item>
+                                <Menu.Item key="4">Tom</Menu.Item>
+                                <Menu.Item key="5">Bill</Menu.Item>
+                                <Menu.Item key="6">Alex</Menu.Item>
+                            </SubMenu>
+                            <SubMenu key="sub2" icon={<TeamOutlined/>} title="Team">
+                                <Menu.Item key="7">Admin</Menu.Item>
+                                <Menu.Item key="8">Registrar</Menu.Item>
+                            </SubMenu>
+                            <Menu.Item key="9" icon={<FileOutlined/>}>
+                                Files
+                            </Menu.Item>
+                        </Menu>
+                    </Sider>
+                    <Layout className="site-layout">
+                        <div className="site-page-header-ghost-wrapper">
+                            <PageHeader
+                                ghost={false}
+                                title="ACME"
+                                subTitle="Student Management System"
+                            >
+                                <Descriptions size="small" column={3}>
+                                    <Descriptions.Item label="Total Number of Students">
+                                        {/*<Tag style={{marginLeft: "10px"}}>Number of students</Tag>*/}
+                                        <Badge count={students.length} className="site-badge-count-4" />
+                                    </Descriptions.Item>
+                                </Descriptions>
+                            </PageHeader>
+                        </div>
+                        {/*<Header className="site-layout-background" style={{padding: 0}}/>*/}
+                        <Content style={{margin: '0 16px'}}>
+                            <Breadcrumb style={{margin: '16px 0'}}>
+                                <Breadcrumb.Item>User</Breadcrumb.Item>
+                                <Breadcrumb.Item>Rommel</Breadcrumb.Item>
+                            </Breadcrumb>
+                            <div className="site-layout-background" style={{padding: 24, minHeight: 360}}>
+                                {renderStudents()}
+                            </div>
+                        </Content>
+                        <Footer style={{textAlign: 'center'}}>
+                            <strong>by medinar <CodeOutlined style={{ fontSize: '18px', color: '#000' }}/></strong> ©2021
+                            <Divider />
+                            <a href="https://www.linkedin.com/in/rommel-medina-17bb9b30/"
+                               target="_blank"
+                               rel="noreferrer noopener"
+                               style={{margin: '5px'}}
+                            >
+                                 <LinkedinOutlined style={{ fontSize: '24px', marginwidth: '10px',color: '#000' }}/>
+                            </a>
+                            <a href="https://github.com/medinar"
+                               target="_blank"
+                               rel="noreferrer noopener"
+                               style={{margin: '5px'}}
+                            >
+                                 <GithubOutlined style={{ fontSize: '24px', color: '#000' }}/>
+                            </a>
+                        </Footer>
+                    </Layout>
+                </Layout>
+            </Fragment>
 }
 
 export default App;
